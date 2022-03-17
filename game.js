313885866f1cd81272bdd74345c00eb23b0ce7c4
Johnatan Santos <jonystrit@gmail.com>
2022-03-17 15:17:14 -0300

    format Document

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // Iniciar variáreis
 
-let board = ['','','','','','','','','']
+let board = ['', '', '', '', '', '', '', '', '']
 let playerTime = 0;
 let symbols = ['o', 'x']
 let gameOver = false
@@ -10,45 +10,47 @@ let vencedor = ''
 let scoreP1 = 0
 let scoreP2 = 0
 let winStates = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-function verificarEmpate(){
-    if(numeroDeJogadas == 9){
+function verificarEmpate() {
+    if (numeroDeJogadas == 9) {
         return true
     }
     return false
 }
-function handleMove(position){
-    if (gameOver == true){
+
+function handleMove(position) {
+    if (gameOver == true) {
         return
     }
-    if (board[position] == ''){
+    if (board[position] == '') {
         board[position] = symbols[playerTime]
         gameOver = isWin()
         numeroDeJogadas++
-        playerTime = (playerTime == 0) ? 1:0 
+        playerTime = (playerTime == 0) ? 1 : 0
     }
     return gameOver
 }
-function isWin(){
-    for(let i in winStates){
+
+function isWin() {
+    for (let i in winStates) {
         let seq = winStates[i]
         let pos1 = seq[0];
         let pos2 = seq[1];
         let pos3 = seq[2];
 
-        if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != ''){
+        if (board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != '') {
             vencedor = board[pos1]
             if (vencedor == 'o') scoreP1++
-            else if(vencedor == 'x') scoreP2++            
+            else if (vencedor == 'x') scoreP2++
             return true
         }
     }
--- interface.js
+++ interface.js
@@ -6,27 +6,28 @@ let winnerPlayer = document.getElementById('winnerPlayer')
 let text = document.getElementById('textFinal')
 let empateText = "Ninguém venceu"
 
-fechar.onclick = function(){
+fechar.onclick = function () {
     modal.style.display = "none";
 }
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll(".square")
-    squares.forEach((square)=>{
+    squares.forEach((square) => {
         square.addEventListener('click', handleClick)
     })
 })
-function handleClick(event){
+
+function handleClick(event) {
     let square = event.target
     let position = square.id
 
-    if(handleMove(position)==true){
-        setTimeout(()=>{
+    if (handleMove(position) == true) {
+        setTimeout(() => {
             modal.style.display = "block"
             winnerPlayer.setAttribute("id", vencedor)
             resetRound()
-        },100)
-    }else if(verificarEmpate() == true){
+        }, 100)
+    } else if (verificarEmpate() == true) {
         console.log('empate')
         modal.style.display = 'block'
         document.getElementById('text').style.display = 'none'
@@ -36,25 +37,28 @@ function handleClick(event){
 
     updateSquare(position)
 }
-function updateSquare(position){
+
+function updateSquare(position) {
     let square = document.getElementById(position.toString())
     let symbol = board[position]
     square.innerHTML = `<div class="${symbol}"></div>`
 }
+
 function resetRound() {
-    board = ['','','','','','','','','']
+    board = ['', '', '', '', '', '', '', '', '']
     numeroDeJogadas = 0
-    for(let position in board){
+    for (let position in board) {
         updateSquare(position)
     }
-    playerTime = (playerTime == 0) ? 1:0
+    playerTime = (playerTime == 0) ? 1 : 0
     gameOver = false
     p1.innerText = scoreP1
     p2.innerText = scoreP2
 }
-function resetGame(){
+
+function resetGame() {
     scoreP1 = 0
     scoreP2 = 0
-    
+
     resetRound()
 }
